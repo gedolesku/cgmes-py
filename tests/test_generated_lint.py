f70268b1998b69b5fdca2ac64ceab1149d9eb3a7
gedolesku <nikolageorgijevic@gmail.com>
2025-06-04 19:39:21 +0200

    izmena testa da ne brise na kraju nego na pocetku

--- tests/test_generated_lint.py
+++ tests/test_generated_lint.py
@@ -4,6 +4,7 @@ from generate_cgmes_project import generate_dataclasses
 
 
 def test_generated_code_with_pylint():
+    shutil.rmtree("generated", ignore_errors=True)
     """Run the generator and lint the result with pylint."""
     generate_dataclasses("cgmes-models/v24/ENTSOE_CGMES_v2.4.15_7Aug2014.xml", "generated")
     try:
@@ -18,5 +19,6 @@ def test_generated_code_with_pylint():
             print(result.stderr)
         assert result.returncode == 0, "pylint found errors"
     finally:
-        shutil.rmtree("generated", ignore_errors=True)
+        pass
+        #shutil.rmtree("generated", ignore_errors=True)
 

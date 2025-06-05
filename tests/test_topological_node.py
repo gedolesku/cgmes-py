c1a182802139ae2f7e7e9f377ce6d8caecc654ad
gedolesku <nikolageorgijevic@gmail.com>
2025-06-06 01:57:39 +0200

    ima neki testovi sto padaju

--- tests/test_topological_node.py
+++ tests/test_topological_node.py
@@ -4,11 +4,13 @@ from runtime.base import parse_dataclass, to_element, parse_file
 from generated.EuropeanStandards.CommonGridModelExchangeStandard.TopologyProfile.Topology.TopologicalNode import TopologicalNode
 
 data_xml = (
-    '<cim:TopologicalNode xmlns:cim="http://iec.ch/TC57/2013/CIM-schema-cim#" '
-    'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">'
-    "<cim:TopologicalNode.name>Node</cim:TopologicalNode.name>"
-    '<cim:TopologicalNode.BaseVoltage rdf:resource="#BV1"/>'
-    "</cim:TopologicalNode>"
+      """
+      <cim:TopologicalNode rdf:ID="_0471bd2a-c766-11e1-8775-005056c00008">
+    <cim:IdentifiedObject.name>HickryCk</cim:IdentifiedObject.name>
+    <cim:TopologicalNode.ConnectivityNodeContainer rdf:resource="#_047eb579-c766-11e1-8775-005056c00008" />
+    <cim:TopologicalNode.BaseVoltage rdf:resource="#_c63f79cc-7953-4ab6-9fa6-f8c729bf895b" />
+  </cim:TopologicalNode>
+  """
 )
 
 
@@ -17,6 +19,8 @@ def test_roundtrip(tmp_path):
     elem = etree.fromstring(data_xml)
     obj = parse_dataclass(elem, TopologicalNode)
     out_elem = to_element(obj)
+    
+    print(f"e1={etree.tostring(out_elem)}\ne2={etree.tostring(elem)}")
     assert etree.tostring(out_elem) == etree.tostring(elem)
 
     # streaming helper
@@ -24,5 +28,5 @@ def test_roundtrip(tmp_path):
     file.write_text(f"<root>{data_xml}</root>")
     objs = list(parse_file(str(file), TopologicalNode))
     assert len(objs) == 1
-    assert objs[0].name == "Node"
-    assert objs[0].BaseVoltage_id == "#BV1"
+    assert objs[0].name == "HickryCk"
+    assert objs[0].BaseVoltage_id == "#_c63f79cc-7953-4ab6-9fa6-f8c729bf895b"
